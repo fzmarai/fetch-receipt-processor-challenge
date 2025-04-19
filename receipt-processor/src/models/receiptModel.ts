cf8346ef384c429470e4efd93ca932bb0fd8d496
Farhad Zmarai <farhad.zmarai@gmail.com>
2025-04-19 12:11:42 -0400

    in memory storage service created using model/repository pattern

---
+++ receipt-processor/src/models/receiptModel.ts
@@ -0,0 +1,17 @@
+import { Receipt } from "@/types/receipt";
+
+// In-memory storage
+const receipts: Map<string, Receipt> = new Map();
+
+export const getReceiptById = async (id: string): Promise<Receipt | null> => {
+    return receipts.get(id) || null;
+};
+
+export const createReceipt = async (id: string, data: Receipt): Promise<Receipt> => {
+    receipts.set(id, data);
+    return data;
+};
+
+export const deleteReceipt = async (id: string): Promise<boolean> => {
+    return receipts.delete(id);
+};
