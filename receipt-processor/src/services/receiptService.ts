5c5fdb53378fbfd78ac2dc72f27ddf53c8e2db26
Farhad Zmarai <farhad.zmarai@gmail.com>
2025-04-19 14:22:48 -0400

    added simple console logging and error handling

--- receipt-processor/src/services/receiptService.ts
+++ receipt-processor/src/services/receiptService.ts
@@ -8,10 +8,18 @@ import * as receiptModel from '@/models/receiptModel';
  */
 
 export const processReceipt = async (receipt: Receipt): Promise<string> => {
-    const id = uuidv4();
-    await receiptModel.createReceipt(id, receipt);
-    return id;
-}
+
+    try {
+        const id = uuidv4();
+        await receiptModel.createReceipt(id, receipt);
+        console.log(`Receipt with ID: ${id} processed successfully`);
+        return id;
+    } catch (error) {
+        console.error('Error processing receipt: ', error);
+        throw error;
+    }
+    
+};
 
 
 /**
@@ -19,16 +27,34 @@ export const processReceipt = async (receipt: Receipt): Promise<string> => {
  */
 
 export const getReceipt = async (id: string): Promise<Receipt | null> => {
-    return await receiptModel.getReceiptById(id);
-}
+    try {
+        const receipt = await receiptModel.getReceiptById(id);
+        if (!receipt) {
+            throw new Error(`Receipt with ID: ${id} not found`);
+        }
+        return receipt;
+    } catch (error) {
+        console.error(`Error retrieving receipt with ID: ${id}`, error);
+        throw error;
+    }
+    
+};
 
 /**
  * Get points for a receipt by ID
  */
 export const getPoints = async (id: string): Promise<number> => {
-    const receipt = await getReceipt(id);
-    if (!receipt) {
-        throw new Error(`Receipt with ID ${id} not found`);
+    try {
+        const receipt = await getReceipt(id);
+        if (!receipt) {
+            throw new Error(`Receipt with ID: ${id} not found`);
+        }
+        const points = calculatePoints(receipt);
+        console.log(`Points calculated for receipt with ID: ${id}: ${points}`);
+        return points;
+    } catch (error) {
+        console.error(`Error calculating points for receipt with ID: ${id}`, error);
+        throw error;
     }
-    return calculatePoints(receipt);
-}
+    
+};
